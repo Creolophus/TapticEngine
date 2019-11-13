7bdd8ab5cb144eec3fb85d91167c139f4fc74795
Creolophus <civilian0304@gmail.com>
2019-11-13 10:06:04 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-Taptic Engine/Pods-Taptic Engine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Taptic_Engine : NSObject
+@end
+@implementation PodsDummy_Pods_Taptic_Engine
+@end
--- Taptic Engine/ViewController.swift
+++ Taptic Engine/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    var buttons : [UIButton]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let data : [(color: UIColor, feedbackType: UINotificationFeedbackGenerator.FeedbackType, title: String)] = [
+            (.systemGreen, .success, "success"),
+            (.systemYellow, .warning, "warning"),
+            (.systemRed, .error, "error")]
+        
+        buttons = data.map { (tuple) -> UIButton in
+            let button = UIButton(type: .system)
+            button.layer.cornerRadius = 40
+            button.backgroundColor = tuple.color
+            button.setTitleColor(UIColor.reverseLabel, for: .normal)
+            button.tag = tuple.feedbackType.rawValue
+            button.setTitle(tuple.title, for: .normal)
+            button.addTarget(self, action: #selector(onTapped), for: .touchUpInside)
+            return button
+        }
+        
+        let stackView = UIStackView(arrangedSubviews: buttons!)
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { (maker) in
+            maker.left.equalTo(view.safeAreaLayoutGuide.snp.left).offset(20)
+            maker.right.equalTo(view.safeAreaLayoutGuide.snp.right).offset(-20)
+            maker.centerY.equalToSuperview()
+            maker.height.equalTo(100)
+        }
+        
+        for button in buttons! {
+            button.snp.makeConstraints { (maker) in
+                maker.size.equalTo(CGSize(width: 80, height: 80))
+            }
+        }
     }
-
-
+    
+    
+    @objc func onTapped(button: UIButton) {
+        let gen = UINotificationFeedbackGenerator()
+        gen.notificationOccurred(UINotificationFeedbackGenerator.FeedbackType(rawValue: button.tag) ?? .success)
+    }
 }
+
+
+extension UIColor {
+    static var reverseLabel: UIColor {
+            if #available(iOS 13, *) {
+                return UIColor { (traitCollection: UITraitCollection) -> UIColor in
+                    if traitCollection.userInterfaceStyle == .dark {
+                        return UIColor.white
+                    } else {
+                        return UIColor.darkText
+                    }
+                }
+            } else {
+                return UIColor.darkText
+            }
+        }
+}
+
 
